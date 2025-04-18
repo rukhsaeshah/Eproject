1d561edeb606e9a1e20716cf51545f7ae9b56b6b
SHAH <SHAH@DESKTOP-FEKFL1U>
2025-04-18 17:28:34 +0500

    Add project files.

---
+++ Areas/Identity/Data/EprojectContext.cs
@@ -0,0 +1,41 @@
+﻿using Eproject.Areas.Identity.Data;
+using Eproject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Eproject.Data;
+
+public class EprojectContext : IdentityDbContext<EprojectUser>
+{
+    public EprojectContext(DbContextOptions<EprojectContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Faculty> faculty { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new EprojectUserEntityConfiguration()); 
+
+    }
+}
+
+internal class EprojectUserEntityConfiguration : IEntityTypeConfiguration<EprojectUser>
+{
+    public void Configure(EntityTypeBuilder<EprojectUser> builder)
+    {
+
+        builder.Property(x => x.FirstName).HasMaxLength(255);
+
+
+        builder.Property(x => x.LastName).HasMaxLength(255);
+
+
+        builder.Property(x => x.Role).HasMaxLength(255).HasDefaultValue('0');
+        //throw new NotImplementedException();
+    }
+}
---
+++ Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Eproject.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eproject.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private readonly SignInManager<EprojectUser> _signInManager;
+        private readonly UserManager<EprojectUser> _userManager;
+
+        public AdminController(SignInManager<EprojectUser> signInManager, UserManager<EprojectUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
